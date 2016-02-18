8c7b726575e273051cbb6bdc10dc5721d7f77f3a
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-02-17 16:05:05 -0800

    Place current location marker on google map

--- map.js
+++ map.js
@@ -2,19 +2,21 @@
   // console.log('fuck');
   navigator.geolocation.getCurrentPosition(function(position) {
     console.log(position.coords.latitude, position.coords.longitude);
-    makeMap(position.coords.latitude, position.coords.longitude);  
+    makeMap({lat: position.coords.latitude, lng: position.coords.longitude});  
   }, function(err) {
     console.error(err);
   });
   // console.log(navigator.geolocation);
 // };
 var map;
-var makeMap = function(lat, lng) {
+var marker;
+var makeMap = function(latLngObj) {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: lat,
-      lng: lng
-    },
-    zoom: 8
+    center: latLngObj,
+    zoom: 15
+  });
+  marker = new google.maps.Marker({
+    position: latLngObj,
+    map: map
   });
 };
