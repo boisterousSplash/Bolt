93dd0e09072806c740ea5f9cca47c4bb21a6fc0a
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-02-18 11:22:51 -0800

    Begin work on BoltController, stargin with getCurrentCoords function

--- client/app/controllers/boltController.js
+++ client/app/controllers/boltController.js
@@ -1,7 +1,18 @@
 angular.module('bolt.controller', [])
 
-.controller('BoltController', function(){
-  // Kyle's code here
-  // Kyle's code here
-  // Kyle's code here
+.controller('BoltController', function($scope, Geo){
+  $scope.currentCoords = {
+    lat: null,
+    lng: null
+  };
+  $scope.getCurrentCoords = function() {
+    console.log('ran');
+    Geo.getCurrentCoords(function(coordsObj) {
+      $scope.currentCoords = coordsObj;
+      console.log('$scope.currentCoords.lat: ', $scope.currentCoords.lat);
+      console.log('$scope.currentCoords.lng: ', $scope.currentCoords.lng);
+    });
+  };
+
+  // setInterval($scope.getCurrentCoords, 500);
 })
