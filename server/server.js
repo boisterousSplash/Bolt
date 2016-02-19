58f51bd50129582d3fdcac7413102ac2108dbd5b
Elliot Plant <elliotaplant@gmail.com>
2016-02-19 14:52:32 -0800

    Remove comments from mongolab setup process

--- server/server.js
+++ server/server.js
@@ -4,19 +4,10 @@ var mongoose = require('mongoose');
 var app = express();
 
 // connect to mongo database named "bolt"
+// uncomment this line to use a local database
 // mongoose.connect('mongodb://localhost/bolt');
 mongoose.connect('mongodb://heroku_l3g4r0kp:61docmam4tnk026c51bhc5hork@ds029605.mongolab.com:29605/heroku_l3g4r0kp');
 
-// mongoose.connect(process.env.MONGOLAB_URI, function (error) {
-//     if (error) console.error(error);
-//     else console.log('mongo connected');
-// });
-// MONGOLAB_URI: mongodb://heroku_l3g4r0kp:61docmam4tnk026c51bhc5hork@ds029605.mongolab.com:29605/heroku_l3g4r0kp
-// Also maybe the MONGOLAB_URI: mongodb://elliotaplant:bolt1738@ds029605.mongolab.com:29605/heroku_l3g4r0kp
-// API key: kyYsvLVM95SVVl9EKZ-dbfX0LzejuVJf
-// uesrname: elliotaplant
-// password: bolt1738
-// configure our server with all the middleware and routing
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
