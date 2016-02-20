e5f98fc9df41286340824ad25498d076b092a604
Elliot Plant <elliotaplant@gmail.com>
2016-02-19 20:06:29 -0800

    Make enter key direct to bolt page when profile is loaded

--- client/app/controllers/createProfileController.js
+++ client/app/controllers/createProfileController.js
@@ -1,7 +1,9 @@
 angular.module('bolt.createProfile', [])
 
-.controller('CreateProfileController', function($scope, Profile) {
+.controller('CreateProfileController', function($location, $scope, Profile) {
   $scope.createProfile = function(first, last, email, phone, distance) {
+    $location.path('/');
+
     var newData = {
       first: first,
       last: last,
