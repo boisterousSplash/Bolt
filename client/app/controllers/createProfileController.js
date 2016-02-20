0c56df1c2118334d78ed8c96468001c32399265a
Shane McGraw <shanemcgraw17@gmail.com>
2016-02-19 19:19:05 -0800

    Remove console.log test

--- client/app/controllers/createProfileController.js
+++ client/app/controllers/createProfileController.js
@@ -2,7 +2,6 @@ angular.module('bolt.createProfile', [])
 
 .controller('CreateProfileController', function($scope, Profile) {
   $scope.createProfile = function(first, last, email, phone, distance) {
-    console.log(arguments);
     var newData = {
       first: first,
       last: last,
