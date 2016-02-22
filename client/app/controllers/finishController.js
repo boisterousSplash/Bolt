9ae91c3f9c4b66a59337d81a1e97738f487572b1
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-02-22 09:35:39 -0800

    Refactor passing of runTime and achievement variables from using sessionStorage to instead using nested controllers with the BoltController acting as the $rootScope

--- client/app/controllers/finishController.js
+++ client/app/controllers/finishController.js
@@ -1,5 +1,4 @@
 angular.module('finish.controller', [])
   .controller('FinishController', function($scope) {
-    $scope.runTime = sessionStorage.getItem('runTime');
-    $scope.achievement = sessionStorage.getItem('achievement');
+    // Code
   });
