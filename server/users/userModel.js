69dd16413fb79c7e8d6dd4330811e50ea899d81c
Shane McGraw <shanemcgraw17@gmail.com>
2016-02-22 12:33:38 -0800

    Add personal best and acheivements to User schema

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -15,7 +15,7 @@ var UserSchema = new mongoose.Schema({
   phone: Number,
   preferedDistance: Number,
   mileSpeed: { type: Number, default: 10 }, // in min/mile
-  runs: Array
+  runs: Array,
   // holds run objects, format should be {
   //   date: Date,
   //   startLocation: {
@@ -30,6 +30,16 @@ var UserSchema = new mongoose.Schema({
   //   actualTime: Number,
   //   medalRecieved: String,
   //   racedAgainst: ObjectID? String?
+  // }
+
+  personalBest: Number, // Personal best in min/mile
+  acheivements: Object
+  // Object of all lifetime medals received in following format:
+  // {
+  //   gold: 23,
+  //   silver: 32,
+  //   bronze: 9,
+  //   iron: 2 -> (this is the experimental medal based on beating someone else's personal best)
   // }
 });
 
