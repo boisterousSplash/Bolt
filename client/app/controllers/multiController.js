f149fc63f1bd1464259051358dbf5016a3b1199c
Erick Paepke <epaepke@gmail.com>
2016-02-23 15:37:51 -0800

    Remove users from geofire database after canceling search

--- client/app/controllers/multiController.js
+++ client/app/controllers/multiController.js
@@ -1,36 +1,37 @@
 angular.module('multi.Controller', ['bolt.profile'])
 
-.controller('MultiController', function($scope, $window, Profile){
+.controller('MultiController', function($scope, $window, $location, Profile){
   $scope.session = $window.localStorage;
+  $scope.currentUser;
+
+  var firebaseRef = new Firebase("https://insane-bolt.firebaseio.com/");
+  var geoFire = new GeoFire(firebaseRef);
 
 	$scope.stopGeoUpdater = function() {
 	  clearInterval($scope.geoUpdater);
 	};
 
-  $scope.addUserGeoFire = function() {
-    var firebaseRef = new Firebase("https://insane-bolt.firebaseio.com/");
-    var geoFire = new GeoFire(firebaseRef);
-    var userPosition;
-
+  $scope.addUserGeoFire = function(){
     navigator.geolocation.getCurrentPosition(function(position) {
-      userPosition = position;
-      
-
       Profile.getUser().then(function(user) {
-        currUser = user;
-        console.log('currUser', currUser);
-
-      geoFire.set(user.data._id, [userPosition.coords.latitude, userPosition.coords.longitude]).then(function() {
-        console.log("Provided key has been added to GeoFire");
-        }, function(error) {
-          console.log("Error: " + error);
+        $scope.currentUser = user.data;
+        geoFire.set(user.data._id, [position.coords.latitude, position.coords.longitude]).then(function() {
+          console.log("Provided key has been added to GeoFire");
+          }, function(error) {
+            console.log("Error: " + error);
           });
-      });
-    
-    }, function(err) {
-      console.error(err);
+        });
+      }, function(err) {
+        console.error(err);
     });
   };
+
+  $scope.cancelSearch = function(){
+    console.log('current user', $scope.currentUser);
+    geoFire.remove($scope.currentUser._id).then(function() {
+    });
+    $location.path('/bolt');
+  }
 
 
   $scope.addUserGeoFire();
