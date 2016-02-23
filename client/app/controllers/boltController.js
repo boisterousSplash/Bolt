03daf638ff91415512fa89b25dfd5282489089dd
Erick Paepke <epaepke@gmail.com>
2016-02-22 22:22:51 -0800

    Route users to solo run page when solo option is selected

--- client/app/controllers/boltController.js
+++ client/app/controllers/boltController.js
@@ -1,5 +1,9 @@
 angular.module('bolt.controller', [])
 
-.controller('BoltController', function($scope, Geo){
-  
+.controller('BoltController', function($scope, $location){
+  $scope.startRun = function() {
+    if (document.getElementById("switch_3_left").checked) {
+      $location.path('/run');
+    }
+  }
 })
