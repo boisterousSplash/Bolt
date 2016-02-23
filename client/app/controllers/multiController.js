0f44de147aedfb21e4a6a6d95768ec2e56aa5889
Erick Paepke <epaepke@gmail.com>
2016-02-23 15:04:42 -0800

    Set up initial connection to geofire database

--- client/app/controllers/multiController.js
+++ client/app/controllers/multiController.js
@@ -1,9 +1,27 @@
-angular.module('multi.Controller', [])
+angular.module('multi.Controller', ['bolt.profile'])
 
-.controller('MultiController', function($scope){
+.controller('MultiController', function($scope, $window, Profile){
+  $scope.session = $window.localStorage;
 
 	$scope.stopGeoUpdater = function() {
 	  clearInterval($scope.geoUpdater);
 	};
 
+  $scope.addUserGeoFire = function() {
+    var firebaseRef = new Firebase("https://insane-bolt.firebaseio.com/");
+    var geoFire = new GeoFire(firebaseRef);
+
+    Profile.getUser().then(function(user) {
+      currUser = user;
+      console.log('currUser', currUser);
+
+    geoFire.set(user.data._id, [37.785326, -122.405696]).then(function() {
+      console.log("Provided key has been added to GeoFire");
+      }, function(error) {
+        console.log("Error: " + error);
+        });
+      });
+    };
+
+  $scope.addUserGeoFire();
 })
