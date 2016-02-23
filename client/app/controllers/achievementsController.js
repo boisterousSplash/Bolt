496719ba7aab9affa3ed38b4991e3bac1301ec1a
Erick Paepke <epaepke@gmail.com>
2016-02-22 19:16:04 -0800

    Animate achievements page using d3 library

--- client/app/controllers/achievementsController.js
+++ client/app/controllers/achievementsController.js
@@ -1,6 +1,25 @@
 angular.module('achievements.controller', [])
   .controller('AchievementsController', function($scope) {
-    $scope.gold = 16;
-    $scope.silver = 8;
-    $scope.bronze = 2;
+    $scope.gold = 15;
+    $scope.silver = 13;
+    $scope.bronze = 5;
+
+    var medalCounts = [$scope.gold.toString(), $scope.silver.toString(), $scope.bronze.toString()];
+
+
+    $scope.incrementCounts = function(width) {
+      selection = d3.select("body").selectAll("span.number").data(medalCounts);
+      selection.transition()
+      .tween("html", function(d) {
+        var i = d3.interpolate(this.textContent, d);
+        return function(t) {
+        this.textContent = Math.round(i(t));
+      };
+      })
+      .duration(1500)
+      .style('width', width + 'px')
+    }
+
+    $scope.incrementCounts();
+    
   });
