52e250bfd3b4f3914093e2ad682bddbced3e4159
Shane McGraw <shanemcgraw17@gmail.com>
2016-02-22 17:42:30 -0800

    Update run object after each run

--- client/app/controllers/runController.js
+++ client/app/controllers/runController.js
@@ -26,6 +26,8 @@ angular.module('run.controller', [])
   var startTime;
   var runTime;
   var statusUpdateLoop;
+  var startLat;
+  var startLong;
 
   function updateTotalRunTime() {
     var minutesRan = moment().diff(startTime, 'minutes');
@@ -111,16 +113,45 @@ angular.module('run.controller', [])
   function finishRun() {
     $rootScope.runTime = runTime.format('mm:ss');
     var medal = $rootScope.achievement = $scope.currentMedal;
+    
+    var date = new Date();
+
+    var endLocation = {
+      latitude: $scope.destination.lat,
+      longitude: $scope.destination.long
+    };
+    var googleExpectedTime = null;
+    var actualTime = runTime;
+
+    var currentRunObject = {
+      date: date,
+      startLocation: {
+        longitude: null,
+        latitude: null
+      },
+      endLocation: {
+        longitude: $scope.destination.long,
+        latitude: $scope.destination.lat
+      },
+      googleExpectedTime: null,
+      actualTime: runTime,
+      medalReceived: medal,
+      racedAgainst: null,
+    };
 
     Profile.getUser()
     .then(function(user) {
       var achievements = user.data.achievements;
+      var previousRuns = user.data.runs;
 
       //update achievments object
       achievements[medal] = achievements[medal] + 1;
+      //update runs object
+      previousRuns.push(currentRunObject);
 
       updatedAchievementsData = {
-        achievements: achievements
+        achievements: achievements,
+        runs: previousRuns
       };
 
       Profile.updateUser(updatedAchievementsData, user)
