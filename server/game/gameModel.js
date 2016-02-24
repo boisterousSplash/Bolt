4a0216a00dee842d2cf1ac7c578041f34f30aae0
Erick Paepke <epaepke@gmail.com>
2016-02-24 15:11:15 -0800

    Remove old files

--- server/game/gameModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var GameSchema = new mongoose.Schema({
-  user1: Object,
-  user2: Object,
-  active: Boolean,
-});
-
-module.exports = mongoose.model('game', GameSchema);
