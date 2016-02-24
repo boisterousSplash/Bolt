ba44345ccdc18430049e40e850dbf1a91bf40a92
Erick Paepke <epaepke@gmail.com>
2016-02-23 17:28:01 -0800

    Use different geofire database for debugging

--- client/app/controllers/multiController.js
+++ client/app/controllers/multiController.js
@@ -12,22 +12,21 @@ angular.module('multi.Controller', ['bolt.profile'])
 })
 
 .factory('Multi', function($location, $interval, Profile) {
-  var firebaseRef = new Firebase("https://insane-bolt.firebaseio.com/");
+  var firebaseRef = new Firebase("https://glowing-fire-8101.firebaseio.com/");
   var geoFire = new GeoFire(firebaseRef);
   var currentUser;
   var userPosition;
   var stop;
 
   var search = function(geoQuery) {
-    console.log('searching...');
-    var users = [];
+    console.log('searching');
     var onKeyEnteredRegistration = geoQuery.on("key_entered", function(key, location, distance) {
       if (key !== currentUser._id) {
         console.log("found match, stop search")
         console.log("user id of new user", key);
         console.log("id of current user", currentUser._id);
-        geoFire.remove(key).then(function() {});
         $interval.cancel(stop);
+        return;
       }
     });
   }
@@ -54,7 +53,7 @@ angular.module('multi.Controller', ['bolt.profile'])
         userPosition = position;
         // addUserGeoFire (this entire function) should start here if the user id and position are accessible
         // from session
-        geoFire.set(user.data._id, [position.coords.latitude, position.coords.longitude]).then(function() {
+        geoFire.set(user.data._id, [userPosition.coords.latitude, userPosition.coords.longitude]).then(function() {
           console.log("Provided key has been added to GeoFire");
           generateQuery();
           }, function(error) {
