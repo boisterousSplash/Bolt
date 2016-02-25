4c8474bbca431be8f1ad8193cf4f09511fb5a646
Elliot Plant <elliotaplant@gmail.com>
2016-02-25 11:29:04 -0800

    Lint files from meeting 2/25

--- server/game/multiGameModel.js
+++ server/game/multiGameModel.js
@@ -4,11 +4,26 @@ var MultiGameSchema = new mongoose.Schema({
   user1: Object,
   user2: Object,
   active: Boolean,
-  id: {type: String, required: true},
-  user1: {type: Boolean, default: false},
-  user2: {type: Boolean, default: false},
-  canceled: {type: Boolean, default: false},
-  won: {type: Boolean, default: false}
+  id: {
+    type: String,
+    required: true
+  },
+  user1: {
+    type: Boolean,
+    default: false
+  },
+  user2: {
+    type: Boolean,
+    default: false
+  },
+  canceled: {
+    type: Boolean,
+    default: false
+  },
+  won: {
+    type: Boolean,
+    default: false
+  }
 });
 
 module.exports = mongoose.model('multiGame', MultiGameSchema);
