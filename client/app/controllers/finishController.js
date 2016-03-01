523733988645176700ca74f4334c388d08f6b194
Shane McGraw <shanemcgraw17@gmail.com>
2016-02-29 16:06:01 -0800

    Add comments documenting function use

--- client/app/controllers/finishController.js
+++ client/app/controllers/finishController.js
@@ -1,5 +1,8 @@
 angular.module('finish.controller', [])
+  // This controller is responsible for the finish.html view
   .controller('FinishController', function ($scope, $location, $route) {
+    // This will set the route to the main /bolt page and give the access
+    // to $scope
     $scope.raceAgain = function () {
       $route.reload('/bolt');
     };
